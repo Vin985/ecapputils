71629e6677ee7290186648c5fd45015d0755f0a8
Vin985 <sylvain.christin@gmail.com>
2017-08-20 22:01:44 -0400

    apputils

--- R/appUtils.R
+++ R/appUtils.R
@@ -1,8 +1,12 @@
 library(shinyjs)
 
+RANDOM_POOL <- c(1:9, letters, toupper(letters))
 
-CONF_VAR <- "EC_APP_CONF"
 REQUIRED_FIELDS <- c("id", "url", "private")
+
+EC_STATUS_GUEST <- 0
+EC_STATUS_USER <- 1
+EC_STATUS_ADMIN <- 2
 
 readAppConf <- function(dir, file = "appconf.csv") {
   conf <-
@@ -14,7 +18,7 @@ readAppConf <- function(dir, file = "appconf.csv") {
   }
   conf$private <- as.logical(conf$private)
   res <- setNames(split(conf[, -1], seq(nrow(conf))), conf$id)
-  assign(CONF_VAR, res, .GlobalEnv)
+  assign("EC_APP_CONF", res, .GlobalEnv)
   res
 }
 
@@ -27,13 +31,73 @@ applicationLink <- function(appId, lang, button = TRUE, ...) {
 }
 
 
-applicationObserver <- function(appId, input, lang) {
-    conf <- get(CONF_VAR)
+generateApplicationURL <- function(app, lang, user) {
+  langArg <- paste0("?lang=", lang)
+  userArg <- ifelse(is.null(user), "", paste0("&user=", user$token))
+  paste0("window.location = '",
+         EC_APP_CONF[[app]]$url,
+         langArg,
+         userArg,
+         "';")
+}
+
+applicationObserver <- function(appId, input, lang, user) {
     shiny::observeEvent(input[[paste0(appId, "App")]], {
-      # create the javascript to redirect to the app with the selected language
-      js <-
-        paste0("window.location = '", conf[[appId]]$url,
-               "?lang=", lang , "';")
+      # create the javascript to redirect to the app with the selected language and user info
+      js <- generateApplicationURL(appId, lang, user)
       shinyjs::runjs(js)
     })
   }
+
+generateUserStatus <- function(status) {
+  random <- c(sample(RANDOM_POOL, 10, replace = TRUE), status)
+  res <- paste(random, collapse = "")
+}
+
+extractUserStatus <- function(str) {
+  l <- nchar(str)
+  res <- substr(str, l, l)
+  as.numeric(res)
+}
+
+isAdmin <- function(user) {
+  return(user$status == EC_STATUS_ADMIN)
+}
+
+isLogged <- function(user) {
+  return(!is.null(user) && (user$status == EC_STATUS_USER || user$status == EC_STATUS_ADMIN))
+}
+
+getLanguageFromQueryString <- function(query, defaultLang = NULL, availableLanguages = c("fr", "en"), ...) {
+  lang <- NULL
+  lg <- query[["lang"]]
+  if (!is.null(lg) && lg %in% availableLanguages) {
+    lang <- lg
+  } else {
+    if (!is.null(defaultLang))
+      lang <- defaultLang
+  }
+  lang
+}
+
+getUserStatusFromQueryString <- function(query, default = EC_STATUS_GUEST, ...) {
+  user <- query[["user"]]
+  if (!is.null(user)) {
+    status <- extractUserStatus(user)
+  } else {
+    token <- ""
+    status <- default
+  }
+  list(token = user, status = status)
+}
+
+getInfoFromQueryString <- function(query, args = c("lang" = getLanguageFromQueryString, "user" = getUserStatusFromQueryString), ...) {
+  n <- length(args)
+  res <- vector(mode = "list", length = n)
+  for (i in 1:n) {
+    res[[i]] <- args[[i]](query, ...)
+  }
+  names(res) <- names(args)
+  res
+}
+
