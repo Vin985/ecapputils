bf5838c8ac6aa6d2bf00f65be79dee93367a6931
Vin985 <sylvain.christin@gmail.com>
2017-10-27 00:13:27 -0300

    session timeout

--- R/appUtils.R
+++ R/appUtils.R
@@ -33,7 +33,8 @@ parseLanguage <- function(url, lang) {
 
 generateApplicationURL <- function(app, lang, user) {
   langArg <- ifelse(is.null(lang), "", paste0("?lang=", lang))
-  userArg <- ifelse(is.null(user), "", paste0("&user=", user$token))
+  nameArg <- ifelse(is.null(lang), "", paste0("?name=", user$name))
+  userArg <- ifelse(is.null(user), "", paste0("&user=", generateUserToken(user)))
   url <- parseLanguage(EC_APP_CONF[[app]]$url, lang)
   paste0("window.location = '",
          url,
@@ -50,31 +51,43 @@ applicationObserver <- function(appId, input, lang, user) {
     }, ignoreInit = TRUE)
   }
 
-generateUserStatus <- function(status) {
-  random <- c(sample(RANDOM_POOL, 10, replace = TRUE), status)
-  res <- paste(random, collapse = "")
+generateUserToken <- function(user) {
+  time <- format(as.numeric(user$time) * 42)
+  name <- paste(strtoi(charToRaw(user$name),16L), collapse = "$")
+  res <- paste(time, user$status, name, sep = "_")
 }
 
 extractUserStatus <- function(str) {
-  l <- nchar(str)
-  res <- substr(str, l, l)
-  as.numeric(res)
+  user <- unlist(strsplit(str, "_"))
+  time <- as.numeric(user[1]) / 42
+  status <- as.numeric(user[2])
+  name <- rawToChar(as.raw(as.numeric(unlist(strsplit(user[3], "$", fixed = TRUE)))))
+  list(status = status, time = time, name = name)
 }
 
-isAdmin <- function(user) {
-  if (is.null(user)) {
+isAdmin <- function(user, sessionTimeout = 3600) {
+  return(isLogged(user, sessionTimeout) && user$status == EC_STATUS_ADMIN)
+}
+
+isLogged <- function(user, sessionTimeout = 3600) {
+  if (is.null(user) || is.na(user$status) || is.na(user$time)) {
     return(FALSE)
   }
-  return(user$status == EC_STATUS_ADMIN)
+  expired <- (as.numeric(Sys.time()) - as.numeric(user$time) > sessionTimeout)
+  return(!expired && user$status > EC_STATUS_GUEST)
 }
 
-isLogged <- function(user) {
-  if (is.null(user)) {
-    return(FALSE)
-  }
-  return(!is.null(user) && !is.na(user$status) && (user$status == EC_STATUS_USER || user$status == EC_STATUS_ADMIN))
-}
-
+#' Title
+#'
+#' @param query
+#' @param defaultLang
+#' @param availableLanguages
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
 getLanguageFromQueryString <- function(query, defaultLang = NULL, availableLanguages = c("fr", "en"), ...) {
   lang <- NULL
   lg <- query[["lang"]]
@@ -87,17 +100,40 @@ getLanguageFromQueryString <- function(query, defaultLang = NULL, availableLangu
   lang
 }
 
-getUserStatusFromQueryString <- function(query, default = EC_STATUS_GUEST, ...) {
+#' Title
+#'
+#' @param query
+#' @param sessionTimeout
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+getUserStatusFromQueryString <- function(query, ...) {
   user <- query[["user"]]
   if (!is.null(user)) {
     status <- extractUserStatus(user)
   } else {
-    token <- ""
-    status <- default
+    status <- list(name = "", status = EC_STATUS_GUEST, time = NA)
   }
-  list(token = user, status = status)
+  c(list(token = user), status)
 }
 
+
+#' getInfoFromQueryString
+#'
+#' Extract information from the query string. See also getLanguageFromQueryString and
+#' getUserStatusFromQueryString
+#'
+#' @param query
+#' @param args
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
 getInfoFromQueryString <- function(query, args = c("lang" = getLanguageFromQueryString, "user" = getUserStatusFromQueryString), ...) {
   n <- length(args)
   res <- vector(mode = "list", length = n)
